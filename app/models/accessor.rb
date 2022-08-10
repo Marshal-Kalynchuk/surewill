45f116c669f29f79ec18dd743cac27fd079b46a7
Marshal <marshalkalynchuk1@gmail.com>
2022-08-10 11:32:24 -0600

    Fixed status mailer in accessor

--- app/models/accessor.rb
+++ app/models/accessor.rb
@@ -8,7 +8,7 @@ class Accessor < ApplicationRecord
   validates :role, inclusion: { in: ACCESSOR_TYPES }
 
   def send_status_change_email
-    if saved_change_to_user_id? 
+    if saved_change_to_email? 
       WillMailer.added_to_will_email(self.will, self).deliver_now 
     elsif saved_change_to_can_release? && self.can_release == true
       WillMailer.releaser_status_email(self.will, self).deliver_now
